c459d8e773bb8143db8dbd1ea115fff9ba864d0a
Ignac Brychta <orbitrek1411@gmail.com>
2024-06-17 17:17:57 +0200

    auto update v0

---
+++ AppUpdater.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Velopack;
+
+namespace ChangeScreenResolution_GUI;
+
+internal static class AppUpdater
+{
+	internal static async Task UpdateMyApp()
+	{
+		var mgr = new UpdateManager("https://the.place/you-host/updates");
+
+		if (!mgr.IsInstalled) return; 
+
+		// check for new version
+		var newVersion = await mgr.CheckForUpdatesAsync();
+		if (newVersion == null)
+			return; // no update available
+
+		// download new version
+		await mgr.DownloadUpdatesAsync(newVersion);
+
+		// install new version and restart app
+		mgr.ApplyUpdatesAndRestart(newVersion);
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
-namespace ChangeScreenResolution_GUI
+using Velopack;
+
+namespace ChangeScreenResolution_GUI;
+
+internal static class Program
 {
-	internal static class Program
+	/// <summary>
+	///  The main entry point for the application.
+	/// </summary>
+	[STAThread]
+	static void Main()
 	{
-		/// <summary>
-		///  The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
-		{
-			// To customize application configuration such as set high DPI settings or default font,
-			// see https://aka.ms/applicationconfiguration.
-			ApplicationConfiguration.Initialize();
-			Application.Run(new MainWindow());
-		}
+		// VelopackApp.Build().Run();
+		// Task update = AppUpdater.UpdateMyApp();
+		// update.Wait();
+		// To customize application configuration such as set high DPI settings or default font,
+		// see https://aka.ms/applicationconfiguration.
+		ApplicationConfiguration.Initialize();
+		Application.Run(new MainWindow());
 	}
 }
